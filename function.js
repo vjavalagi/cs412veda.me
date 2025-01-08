f91da6638288e42187195bf2834d6374e907b62d
vjavalagi <107204440+vjavalagi@users.noreply.github.com>
2025-01-08 18:38:26 +0800

    secure website

---
+++ function.js
@@ -0,0 +1,25 @@
+
+function toggleStyleSheet(){
+    var style = document.getElementById("mainStyleSheet");
+    var selectedStyleSheet = style.getAttribute("href")
+    if (style.getAttribute("href") == "bowstyle.css") {
+        style.setAttribute("href", "style.css");
+    } else {
+        style.setAttribute("href", "bowstyle.css");
+    }
+    localStorage.setItem('selectedStyleSheet', style.getAttribute("href")); // save updated in the selected local var 
+
+}
+
+
+window.onload = function(){
+    
+    var storedStyle = localStorage.getItem('selectedStyleSheet');
+    var style = document.getElementById('mainStyleSheet');
+
+    if (storedStyle) { // sets page only if initial design is chosen 
+        style.setAttribute("href", storedStyle);
+    }
+}
+
+
